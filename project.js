7042d02fa2f2d6619736fe3549a4f7f3275ab277
Kyaw Thu <evanch98@gmail.com>
2022-02-13 00:32:31 +0800

    Edit project.js

--- project.js
+++ project.js
@@ -1,7 +1,30 @@
 function computerPlay() {
-    const choice = ["Rock", "Paper", "Scissors"];
+    const choice = ["rock", "paper", "scissors"];
     let random_index = parseInt((Math.random() * 3));
     return choice[random_index];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let result;
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        result = "You Win! Rock beats Scissors.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        result = "You Win! Paper beats Rock.";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        result = "You Win! Scissors beats Paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        result = "You Lose! Rock beats Scissors";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        result = "You Lose! Paper beats Rock.";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        result = "You Lose! Scissors beats Paper.";
+    } else {
+        result = "It's a tie!"
+    }
+    return result;
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
